ee578d46da290b3b3bf1006da934ae6ed9d3c7bd
CoderRx001 <carrillo964@gmail.com>
2018-09-13 19:07:27 -0500

    week 1

---
+++ math.py
@@ -0,0 +1,17 @@
+# + addition
+# - subtract
+# / division
+# * multiply
+# ** exponentiation
+# %  modulize
+# < less than
+# > more than 
+
+print ("what is the the meaning to life, the universe, and everything?", (40 + 30 - 7) * 2 / 3)
+
+print (" is it true that 5 * 2 > 3 * 4?")
+
+print( 5 * 2 > 3 * 4 )
+
+print("what is 5 * 2", 5 * 2)
+print("what is 3 * 4", 3 * 4)
