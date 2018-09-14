6dbd878a299251e6b3cb37c337e2c841ad5e3342
CoderRx001 <carrillo964@gmail.com>
2018-09-13 22:28:50 -0500

    week 1

---
+++ strings.py
@@ -0,0 +1,14 @@
+# strings are text surrounded by quotes
+# can use single '' double "" or triple """ quotes are used
+#i.e. "2112", 'dinosaurs'
+
+kanye_quote = "my great pain is that I will never see myself live"
+
+print (kanye_quote)
+
+# """ triple quotes allows any string to be on multiple lines
+
+hamilton_quote = """ The people said, "this kid's is insane" """
+
+# \' allows for an apostrophe
+# \t creates a tab button space
